6ead102761cdf5d10360d0d2d094eeef82c5c259
AlexSearha <contact@mytechcompanion.fr>
2023-12-04 16:16:44 +0100

    slider + theme ok

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,6 +1,6 @@
 import { createTheme } from '@mui/material/styles';
 
-const theme = createTheme({
+const themeInput = createTheme({
   components: {
     // Inputs
     MuiOutlinedInput: {
@@ -21,4 +21,41 @@ const theme = createTheme({
   },
 });
 
-export default theme;
+const customTabs = createTheme({
+  components: {
+    MuiTabs: {
+      styleOverrides: {
+        root: {
+          border: '1px solid #EBEBEB',
+          borderRadius: 25,
+          fontWeight: 'bold',
+          backgroundColor: '#FAFAFA',
+          overflow: 'hidden',
+        },
+        indicator: {
+          display: 'none',
+        },
+      },
+    },
+    MuiTab: {
+      styleOverrides: {
+        root: {
+          margin: '5px',
+          borderRadius: 25,
+          textTransform: 'none',
+          fontWeight: 'bold',
+        },
+        selected: {
+          boxShadow: '1px 1px 3px #00000066',
+          fontWeight: 'bold',
+          backgroundColor: 'white',
+          '&.Mui-focusVisible': {
+            outline: 'none',
+          },
+        },
+      },
+    },
+  },
+});
+
+export { themeInput, customTabs };
